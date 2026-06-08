0c80f7da68d3f555d3050cdc10959fadb3556301
samtimeinc <samtime.worldwide@gmail.com>
2026-06-07 20:14:57 -0700

    feat: add UI elements for /result

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,17 +1,46 @@
+import CameraIcon from "@/components/cameraIcon";
 import styles from "./page.module.css"
+import GalleryIcon from "@/components/galleryIcon";
 
 
 const ResultPage = () => {
   return (
     <main className={styles["main"]}>
-        <div>
-            <div className={styles["start-message"]}>TO START ANALYSIS</div>
-            <div>
-                <p>Preview</p>
-                <div>[ Add image preview here ]</div>
-            </div>
-
+      <div className={styles["preview-wrapper"]}>
+        <div className={styles["start-message"]}>TO START ANALYSIS</div>
+        <div className={styles["preview"]}>
+          <p className={styles["preview-title"]}>Preview</p>
+          <div className={styles["preview-image"]}></div>
         </div>
+      </div>
+      <div className={styles["access-options"]}>
+        <div className={styles["camera-access"]}>
+          <figure className={styles["icon-wrapper"]}>
+            <CameraIcon />
+            <div className={styles["diamond-one"]}></div>
+            <div className={styles["diamond-two"]}></div>
+            <div className={styles["diamond-three"]}></div>
+          </figure>
+          <div className={styles["camera-subtitle"]}>
+            ALLOW AI
+            <br />
+            TO SCAN YOUR FACE
+          </div>
+        </div>
+        <div className={styles["gallery-access"]}>
+          <figure className={styles["icon-wrapper"]}>
+            <GalleryIcon />
+            <div className={styles["diamond-one"]}></div>
+            <div className={styles["diamond-two"]}></div>
+            <div className={styles["diamond-three"]}></div>
+          </figure>
+          <div className={styles["gallery-subtitle"]}>
+            ALLOW AI
+            <br />
+            TO ACCESS GALLERY
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
